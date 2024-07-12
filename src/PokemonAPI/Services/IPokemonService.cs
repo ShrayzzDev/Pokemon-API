70877d492281d23fa192102466164bccdddfef3d
Renaud Beuret <renaud.beuret@somab.fr>
2024-07-12 09:15:49 +0200

    [ADD] IPokemonServices

---
+++ src/PokemonAPI/Services/IPokemonService.cs
@@ -0,0 +1,20 @@
+﻿namespace Services
+{
+    public interface IPokemonService<T>
+    {
+        /// <summary>
+        /// Get a pokemon from it's pokedex entry
+        /// </summary>
+        /// <param name="id">Pokedex entry of that pokemon</param>
+        /// <returns></returns>
+        public Task<T?> GetPokemonById(int id);
+
+        /// <summary>
+        /// Returns a list a pokemon containing the
+        /// string passed as parameter
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public Task<IEnumerable<T>> GetPokemonByName(string name, int index, int count);
+    }
+}
