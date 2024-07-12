43c986e4f17d938142e7c040e91eefb16b68d5e1
Renaud Beuret <renaud.beuret@somab.fr>
2024-07-12 09:20:51 +0200

    [ADD] Business Logic

---
+++ src/PokemonAPI/Business/PokemonBusinessServices.cs
@@ -0,0 +1,23 @@
+﻿using Entities;
+using EntityExtensions;
+using Model;
+using Services;
+
+namespace Business
+{
+    public class PokemonBusinessServices : IPokemonService<Pokemon>
+    {
+        private readonly IPokemonService<PokemonEntity> _repository;
+
+        public PokemonBusinessServices(IPokemonService<PokemonEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Pokemon?> GetPokemonById(int id)
+            => (await _repository.GetPokemonById(id))?.ToModel();
+
+        public async Task<IEnumerable<Pokemon>> GetPokemonByName(string name, int index, int count)
+            => (await _repository.GetPokemonByName(name, index, count)).ToModels();
+    }
+}
