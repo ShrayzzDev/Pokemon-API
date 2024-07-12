56889a76e9524b3acbeb50dad9ea18a54cc82363
Renaud Beuret <renaud.beuret@somab.fr>
2024-07-12 09:13:51 +0200

    [ADD] Context + Migration

---
+++ src/PokemonAPI/Context/PokemonDBContext.cs
@@ -0,0 +1,32 @@
+﻿using Entities;
+using InitDatabase;
+using Microsoft.EntityFrameworkCore;
+
+namespace Context
+{
+    public class PokemonDBContext : DbContext
+    {
+
+        public DbSet<TypeEntity> Types { get; set; }
+
+        public DbSet<MoveEntity> Moves { get; set; }
+
+        public DbSet<PokemonMoveEntity> MovePool { get; set; }
+
+        public DbSet<PokemonEntity> Pokemons { get; set; }
+
+        public PokemonDBContext() { }
+
+        public PokemonDBContext(DbContextOptions<PokemonDBContext> options) : base(options) { }
+
+        /// <inheritdoc/>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlite($"Data Source=Database_Test.db");
+            }
+        }
+    }
+}
