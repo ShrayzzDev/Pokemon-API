18a06272d0f20bd7e18d8d7ff104cb6b2f106f0d
Renaud Beuret <renaud.beuret@somab.fr>
2024-07-12 09:17:02 +0200

    [ADD] GenericMapper

---
+++ src/PokemonAPI/GenericMapper/GenericEntityMapper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMapper
+{
+    public class GenericEntityMapper<Model, Entity> where Model : class
+                                                    where Entity : class
+    {
+        private readonly GenericMapper<Model, Entity> _mapper = new GenericMapper<Model, Entity>();
+
+        /// <summary>
+        /// Retourne un model
+        /// </summary>
+        /// <param name="e">Entité à chercher</param>
+        /// <returns>Model associé, null si non trouvé</returns>
+        public Model? GetModel(Entity e)
+        {
+            return _mapper.GetT(e);
+        }
+
+        /// <summary>
+        /// Retourne une entité
+        /// </summary>
+        /// <param name="e">Model à chercher</param>
+        /// <returns>Entité associé, null si non trouvé</returns>
+        public Entity? GetEntity(Model m)
+        {
+            return _mapper.GetU(m);
+        }
+
+        /// <summary>
+        /// Ajoute un couple Entité/Model
+        /// </summary>
+        /// <param name="m">Model à ajouter</param>
+        /// <param name="e">Entité à ajouter</param>
+        /// <returns></returns>
+        public bool Add(Model m, Entity e)
+        {
+            return _mapper.Add(e, m);
+        }
+
+        /// <summary>
+        /// Reset tout les éléments du mapper
+        /// </summary>
+        public void Reset()
+        {
+            _mapper.Reset();
+        }
+    }
+}
---
+++ src/PokemonAPI/GenericMapper/GenericMapper.cs
@@ -0,0 +1,63 @@
+﻿namespace GenericMapper
+{
+    internal class GenericMapper<T, U> where U : class
+                                       where T : class
+    {
+        private readonly List<Tuple<T, U>> _items = new List<Tuple<T, U>>();
+
+        /// <summary>
+        /// Retourne un U à partir d'un T
+        /// </summary>
+        /// <param name="t">T à chercher</param>
+        /// <returns>U, null si pas trouvé</returns>
+        public U? GetU(T t)
+        {
+            var tuple = _items.Find(tu => ReferenceEquals(t, tu.Item1));
+            if (tuple == null)
+            {
+                return null;
+            }
+            return tuple.Item2;
+        }
+
+        /// <summary>
+        /// Retourne un T à partir d'un U
+        /// </summary>
+        /// <param name="t">U à chercher</param>
+        /// <returns>T, null si pas trouvé</returns>
+        public T? GetT(U u)
+        {
+            var tuple = _items.Find(tu => ReferenceEquals(u, tu.Item2));
+            if (tuple == null)
+            {
+                return null;
+            }
+            return tuple.Item1;
+        }
+
+        /// <summary>
+        /// Ajoute un couple de U/T
+        /// </summary>
+        /// <param name="u">U à ajouter</param>
+        /// <param name="t">T à ajouter</param>
+        /// <returns>Si au moins un des éléments est déjà présent</returns>
+        public bool Add(U u, T t)
+        {
+            if (GetU(t) != null || GetT(u) != null)
+            {
+                return false;
+            }
+            var tuple = new Tuple<T, U>(t, u);
+            _items.Add(tuple);
+            return _items.Contains(tuple);
+        }
+
+        /// <summary>
+        /// Reset tout les couples du mapper
+        /// </summary>
+        public void Reset()
+        {
+            _items.Clear();
+        }
+    }
+}
