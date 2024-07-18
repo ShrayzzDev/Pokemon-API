a4897acad42e1cfc97ce6ec9ad49ebb24d600730
Renaud Beuret <renaud.beuret@somab.fr>
2024-07-18 10:54:47 +0200

    [ADD] TypeEfficiency service interface

---
+++ src/PokemonAPI/Services/ITypeEfficiencyService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface ITypeEfficiencyService<T> where T : class
+    {
+        /// <summary>
+        /// Gets the damage coefficients 
+        /// </summary>
+        /// <param name="attackType">Type of the attack.</param>
+        /// <param name="targets">Types recieving the attack.</param>
+        /// <returns></returns>
+        public Task<IEnumerable<float>> GetEfficiencyOn(string attackType, IEnumerable<string> targets);
+    }
+}
