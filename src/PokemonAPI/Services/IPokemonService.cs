8e4b2c6ae3ecb2d2751130e4f297572887d03313
Renaud Beuret <renaud.beuret@somab.fr>
2024-07-18 10:53:24 +0200

    [ADD] Now needs the different pokemon types

--- src/PokemonAPI/Services/IPokemonService.cs
+++ src/PokemonAPI/Services/IPokemonService.cs
@@ -1,20 +1,22 @@
 ﻿namespace Services
 {
-    public interface IPokemonService<T>
+    public interface IPokemonService<Detailed, WithoutMoves ,Simple> where Detailed: class
+                                                                     where WithoutMoves: class
+                                                                     where Simple : class
     {
         /// <summary>
         /// Get a pokemon from it's pokedex entry
         /// </summary>
         /// <param name="id">Pokedex entry of that pokemon</param>
-        /// <returns></returns>
-        public Task<T?> GetPokemonById(int id);
+        /// <returns>The pokemon, null if not found</returns>
+        public Task<Detailed?> GetPokemonById(int id);
 
         /// <summary>
         /// Returns a list a pokemon containing the
         /// string passed as parameter
         /// </summary>
         /// <param name="name"></param>
-        /// <returns></returns>
-        public Task<IEnumerable<T>> GetPokemonByName(string name, int index, int count);
+        /// <returns>The pokemon, null if not found</returns>
+        public Task<IEnumerable<Simple>> GetPokemonByName(string name, int index, int count);
     }
 }
