ab97dcd10b46711cf6584d19bae19a43d42deb1d
Renaud Beuret <renaud.beuret@somab.fr>
2024-07-18 10:50:15 +0200

    [ADD] Shadow type and now returns a dict

--- src/PokemonAPI/InitDatabase/InitTypes.cs
+++ src/PokemonAPI/InitDatabase/InitTypes.cs
@@ -5,29 +5,29 @@ namespace InitDatabase
 {
     public static class InitTypes
     {
-        public static IEnumerable<TypeEntity> GetTypesArray()
+        public static Dictionary<int, TypeEntity> GetTypesDict()
         {
-            return new List<TypeEntity>()
-            {
-                new TypeEntity("Normal", "Phy"),
-                new TypeEntity("Fighting", "Phy"),
-                new TypeEntity("Flying", "Phy"),
-                new TypeEntity("Poison", "Phy"),
-                new TypeEntity("Ground", "Phy"),
-                new TypeEntity("Rock", "Phy"),
-                new TypeEntity("Bug", "Phy"),
-                new TypeEntity("Ghost", "Phy"),
-                new TypeEntity("Steel", "Phy"),
-                new TypeEntity("Fire", "Spe"),
-                new TypeEntity("Water", "Spe"),
-                new TypeEntity("Grass", "Spe"),
-                new TypeEntity("Electric", "Spe"),
-                new TypeEntity("Psychic", "Spe"),
-                new TypeEntity("Ice", "Spe"),
-                new TypeEntity("Dragon", "Spe"),
-                new TypeEntity("Dark", "Phy"),
-                new TypeEntity("Fairy", "None")
-            };
+            var dict = new Dictionary<int, TypeEntity>();
+            dict.Add(0, new TypeEntity("Normal", "Phy"));
+            dict.Add(1, new TypeEntity("Fighting", "Phy"));
+            dict.Add(2, new TypeEntity("Flying", "Phy"));
+            dict.Add(3, new TypeEntity("Poison", "Phy"));
+            dict.Add(4, new TypeEntity("Ground", "Phy"));
+            dict.Add(5, new TypeEntity("Rock", "Phy"));
+            dict.Add(6, new TypeEntity("Bug", "Phy"));
+            dict.Add(7, new TypeEntity("Ghost", "Phy"));
+            dict.Add(8, new TypeEntity("Steel", "Phy"));
+            dict.Add(9, new TypeEntity("Fire", "Spe"));
+            dict.Add(10, new TypeEntity("Water", "Spe"));
+            dict.Add(11, new TypeEntity("Grass", "Spe"));
+            dict.Add(12, new TypeEntity("Electric", "Spe"));
+            dict.Add(13, new TypeEntity("Psychic", "Spe"));
+            dict.Add(14, new TypeEntity("Ice", "Spe"));
+            dict.Add(15, new TypeEntity("Dragon", "Spe"));
+            dict.Add(16, new TypeEntity("Dark", "Phy"));
+            dict.Add(17, new TypeEntity("Fairy", "None"));
+            dict.Add(10001, new TypeEntity("Shadow", "None"));
+            return dict;
         }
     }
 }
