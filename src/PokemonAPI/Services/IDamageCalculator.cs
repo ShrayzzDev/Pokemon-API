4be24fc37e474b23a9e2f50f1f00acb86c1a520e
Renaud Beuret <renaud.beuret@somab.fr>
2024-07-18 10:54:07 +0200

    [ADD] DamageCalculator interface

---
+++ src/PokemonAPI/Services/IDamageCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    /// <summary>
+    /// Interface that defines method needed to calculate
+    /// damages.
+    /// </summary>
+    /// <typeparam name="T">Damage Parameters</typeparam>
+    public interface IDamageCalculator<T> where T : class 
+    {
+        /// <summary>
+        /// Calculate damages
+        /// </summary>
+        /// <param name="informations">Parameters used for calculation</param>
+        /// <returns>The damage value</returns>
+        public Task<double> GetDamage(T informations);
+
+        /// <summary>
+        /// Calculate the concrete stat value used
+        /// for calculations.
+        /// </summary>
+        /// <param name="bst">Base stat</param>
+        /// <param name="evs">Amount of evs</param>
+        /// <param name="ivs">Amount of ivs</param>
+        /// <param name="level">Level of the pokemon</param>
+        /// <returns>Real value</returns>
+        protected int EffectiveStat(int bst, int evs, int ivs, int level, bool isHp);
+    }
+}
