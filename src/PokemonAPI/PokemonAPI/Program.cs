41737e34fd1f93df5eb5b355fbe10029aefc2455
Renaud Beuret <renaud.beuret@somab.fr>
2024-07-18 11:05:18 +0200

    [ADD] Necessary DIs

--- src/PokemonAPI/PokemonAPI/Program.cs
+++ src/PokemonAPI/PokemonAPI/Program.cs
@@ -1,9 +1,13 @@
 using Business;
 using Context;
 using Entities;
+using Entities.Pokemons;
+using EntityExtensions.Pokemons;
 using InitDatabase;
 using Microsoft.EntityFrameworkCore;
 using Model;
+using Model.DamageParameters;
+using Model.Pokemons;
 using Repositories;
 using Services;
 
@@ -16,25 +20,28 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IPokemonService<PokemonEntity>, PokemonRepository>();
-builder.Services.AddScoped<IPokemonService<Pokemon>, PokemonBusinessServices>();
+builder.Services.AddScoped<IPokemonService<PokemonEntity, PokemonWithoutMovesEntity, SimplePokemonEntity>, PokemonRepository>();
+builder.Services.AddScoped<IPokemonService<Pokemon, PokemonWithoutMoves, SimplePokemon>, PokemonBusinessServices>();
+
+builder.Services.AddScoped<IMoveService<MoveEntity>, MoveRepository>();
+builder.Services.AddScoped<IMoveService<Move>, MoveBusinessServices>();
+
+builder.Services.AddScoped<ITypeEfficiencyService<TypeEfficiencyEntity>, TypeEfficiencyRepository>();
+
+builder.Services.AddScoped<IDamageCalculator<Gen1DamageInformations>, Gen1DamageCalculator>();
 
 builder.Services.AddDbContext<PokemonDBContext>();
 
-
 using (var context = new PokemonDBContext())
 {
-    foreach (var pkmn in context.Pokemons)
-    {
-        Console.WriteLine(pkmn.Name);
-    }
-    Console.WriteLine(!context.Pokemons.Any());
     if (!context.Pokemons.Any())
     {
-        await context.Types.AddRangeAsync(InitTypes.GetTypesArray());
+        await context.Types.AddRangeAsync(InitTypes.GetTypesDict().Values);
+        await context.SaveChangesAsync();
+        await context.TypeEfficiencies.AddRangeAsync(InitTypeEfficiency.GetTypeEfficiencyArray());
         await context.Moves.AddRangeAsync(InitMoves.GetMovesArray());
-        await context.MovePool.AddRangeAsync(InitPokemonMove.GetPokemonMoveArray());
-        await context.Pokemons.AddRangeAsync(InitPokemon.GetPokemonArray());
+        await context.Pokemons.AddRangeAsync(await InitPokemon.GetPokemonArray(context));
+        await context.MovePool.AddRangeAsync(await InitPokemonMove.GetPokemonMoveArray(context));
 
         await context.SaveChangesAsync();
     }
