bfbcfec6317117ce0accbd97ea7b7e094ffad555
Renaud Beuret <renaud.beuret@somab.fr>
2024-07-18 10:33:16 +0200

    [ADD] DamageParameter Extensions

---
+++ src/PokemonAPI/DTOExtensions/DamageParameters/Gen1DamageInformationsDTOExtensions.cs
@@ -0,0 +1,29 @@
+﻿using DTO.DamageParameters;
+using Model.DamageParameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOExtensions.DamageParameters
+{
+    public static class Gen1DamageInformationsDTOExtensions
+    {
+        public static Gen1DamageInformations ToModel(this Gen1DamageInformationsDTO dto)
+        {
+            return new Gen1DamageInformations(
+                dto.TargetIvs.ToModel(true),
+                dto.TargetEvs.ToModel(false),
+                dto.DamagingIvs.ToModel(true),
+                dto.DamagingEvs.ToModel(false),
+                dto.DamagingId,
+                dto.DamagingLevel,
+                dto.TargetId,
+                dto.TargetLevel,
+                dto.MoveId,
+                dto.IsCritical
+            );
+        }
+    }
+}
---
+++ src/PokemonAPI/DTOExtensions/DamageParameters/Gen1EIValuesExtentions.cs
@@ -0,0 +1,25 @@
+﻿using DTO.DamageParameters;
+using Model.DamageParameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOExtensions.DamageParameters
+{
+    internal static class Gen1EIValuesExtentions
+    {
+        public static Gen1EIValues ToModel(this Gen1EIValuesDTO dto, bool isIvs)
+        {
+            return new Gen1EIValues(
+                dto.HP,
+                dto.Attack,
+                dto.Defense,
+                dto.Special,
+                dto.Speed,
+                isIvs
+            );
+        }
+    }
+}
